055badda5e3f12e3b2ebef3d5774a5e70127801c
Nuwan Tissera <nuwan.tissera@my.sliit.lk>
2018-07-01 16:32:58 +0530

    tutorial samples

---
+++ public/src/app/test/test.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+
+  public name = "Vishwas";
+  public event =""
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  greetUser(){
+    return "Greet";
+  }
+
+  onclick(event){
+    console.log(event)
+    this.event=event.type;
+  }
+
+  public sucessclass = "false";
+  public hasError = "false";
+}
